d37b6b3883766ee954e695dcff17fd700cc563e1
Sumit Bisht <sumit7beazt@gmail.com>
2023-12-21 22:58:01 +0530

    added mongodb database

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -29,7 +29,6 @@ export const authOptions = {
                 } else {
                     // If you return null then an error will be displayed advising the user to check their details.
                     return null
-
                     // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
                 }
             }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,53 @@
 import { useSession, signIn, signOut } from "next-auth/react"
+import clientPromise from "../db"
 
-const HomePage = () => {
+export const getServerSideProps = async () => {
+  try {
+    const client = await clientPromise
+    const db = client.db('test');
+    const collection = db.collection('posts')
+    const docs = await collection.find().toArray();
+
+    return {
+      props: {
+        isConnected: true,
+        docs: JSON.parse(JSON.stringify(docs))
+      },
+    }
+  } catch (e) {
+    console.error(e)
+    return {
+      props: { isConnected: false },
+    }
+  }
+}
+
+const HomePage = ({ isConnected, docs }) => {
   const { data: session } = useSession()
-  // console.log(session)
+  console.log(docs)
 
   return (
     <div style={{ height: '100vh', display: 'flex' }}>
       <div style={{ margin: 'auto' }}>
 
+        {isConnected ? (
+          <h2 >You are connected to MongoDB</h2>
+        ) : (
+          <h2 >
+            You are NOT connected to MongoDB. Check the <code>README.md</code>{' '}
+            for instructions.
+          </h2>
+        )}
+
         <h1>Hello, {session ? session.user.name : 'User'} </h1>
+
         {session ? (
-          <button onClick={() => signOut()}>Sign out</button>
+          <div>
+            <button onClick={() => signOut()}>Sign out</button>
+          </div>
         ) : (
           <button onClick={() => signIn()}>Sign in</button>
-        )
-        }
+        )}
 
       </div>
     </div>
