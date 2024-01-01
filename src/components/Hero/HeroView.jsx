caa6bfda9785f023c35db7aa1fda50fd7ed5ff50
Sumit Bisht <sumit7beazt@gmail.com>
2024-01-01 23:43:58 +0530

    tried shadcn ui components

--- src/components/Hero/HeroView.jsx
+++ src/components/Hero/HeroView.jsx
@@ -1,10 +1,22 @@
+'use client'
 import { Code, Text } from '@radix-ui/themes'
+import { Button } from '../ui/button'
+import { signOut } from 'next-auth/react'
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
 
 const HeroView = () => {
   return (
     <>
       <Text as="p"></Text>
       <Code>console.log('Hero View')</Code>
+      <Button onClick={() => signOut()}>Sign Out</Button>
+      <Alert variant="destructive">
+        {/* <Terminal className="h-4 w-4" /> */}
+        <AlertTitle>Heads up!</AlertTitle>
+        <AlertDescription>
+          Project work in progress
+        </AlertDescription>
+      </Alert>
     </>
   )
 }
